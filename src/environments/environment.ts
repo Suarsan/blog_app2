519c512a7671cb7e8d7500d2a68186210bad8246
Javier Suárez <javiersuarsan@gmail.com>
2023-06-25 13:10:48 -0400

    [env] develop env to production api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiURL: 'http://192.168.1.172:5000/graphql'
+  // apiURL: 'http://192.168.1.172:5000/graphql'
+  apiURL: 'https://api.descubrelavera.com'
 };
