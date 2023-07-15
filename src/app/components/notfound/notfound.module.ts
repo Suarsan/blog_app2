025b0231c2bb4de542d619f08b178230f2701328
Javier Suárez <javiersuarsan@gmail.com>
2023-07-15 00:57:14 -0400

    [fix] not found page styles

--- src/app/components/notfound/notfound.module.ts
+++ src/app/components/notfound/notfound.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotfoundComponent } from './notfound.component';
 import { FooterModule } from '../footer/footer.module';
+import { NavModule } from '../nav/nav.module';
 
 @NgModule({
   declarations: [NotfoundComponent],
   imports: [
     CommonModule,
-    FooterModule
+    FooterModule,
+    NavModule
   ],
   exports: [NotfoundComponent]
 })
