fc2d0148ff5d7bf995f6610efd26fa72de5539dc
Javier Suárez <javiersuarsan@gmail.com>
2023-07-17 19:43:40 -0400

    [refactor] simplify app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,8 @@
-import { isPlatformBrowser } from '@angular/common';
-import { Component, OnDestroy, Inject, PLATFORM_ID } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
-
-declare var gtag;
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnDestroy {
-
-  routerSubscription: Subscription;
-
-  constructor(@Inject(PLATFORM_ID) private platformId: object,
-              private router: Router) {
-                this.routerSubscription = this.router.events.subscribe(event => {
-                  if (event instanceof NavigationEnd && isPlatformBrowser(this.platformId)) {
-                      gtag('config', 'G-QKS5KT6XYE', { page_path: event.urlAfterRedirects });
-                  }
-                });
-  }
-
-  ngOnDestroy() {
-    this.routerSubscription.unsubscribe();
-  }
-}
+export class AppComponent { }
