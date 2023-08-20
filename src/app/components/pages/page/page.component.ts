6849032d64b8a43fddf0b54837d7690da7188f34
Javier Suárez <javiersuarsan@gmail.com>
2023-08-20 12:57:34 -0400

    [fix] fix and refactor set data markups

--- src/app/components/pages/page/page.component.ts
+++ src/app/components/pages/page/page.component.ts
@@ -28,8 +28,7 @@ export class PageComponent implements OnInit {
               private state: TransferState) { }
 
   ngOnInit() {
-    this.router.routeReuseStrategy.shouldReuseRoute = (fut, curr) =>  { 
-     
+    this.router.routeReuseStrategy.shouldReuseRoute = (fut, curr) =>  {
       // @ts-ignore
       return fut._routerState.url.replace(/#.*/, "") === curr._routerState.url.replace(/#.*/, "");
     };
@@ -64,7 +63,22 @@ export class PageComponent implements OnInit {
   
   private _setStructuredData(post) {
     const markupJSONs = [];
-    const breadcrumbListJSON = [{
+
+    // set Exploration markups
+    markupJSONs.push(this.getExplorationMarkups(post));
+    
+    //set Faqs markups
+    const faqs = this.post.paragraphs.filter(p => p.htmlTag.content === 'faq');
+    console.dir(faqs)
+    if (faqs?.length) {
+      markupJSONs.push(this.getFaqsMarkups(faqs));
+    }
+
+    this.seoService.setJSONLDMarkups(markupJSONs);
+  }
+
+  private getExplorationMarkups(post) {
+    const markups = [{
       "@context": "https://schema.org",
       "@type": "BreadcrumbList",
       "itemListElement": [{
@@ -75,34 +89,33 @@ export class PageComponent implements OnInit {
       }]
     }];
     if (post.parent) {
-      breadcrumbListJSON[0].itemListElement.push({
+      markups[0].itemListElement.push({
         "@type": "ListItem",
         "position": 2,
         "name": post.parent.title,
         "item": 'https://descubrelavera.com/' + post.slug + '/' + post.parent.slug
       })
     }
-    markupJSONs.push(breadcrumbListJSON);
-    if (this.post.faqs?.length) {
-      const faqsJSON = {
-        "@context": "https://schema.org",
-        "@type": "FAQPage",
-        "mainEntity": []
-      }
-      for (let faq of this.post.faqs) {
-        faqsJSON.mainEntity.push({
-          "@type": "Question",
-          "name": faq.question,
-          "acceptedAnswer": {
-            "@type": "Answer",
-            "text": faq.answer
-          }
-        });
-      }
-      markupJSONs.push(faqsJSON);
-    }
+    return markups;
+  }
 
-    this.seoService.setJSONLDMarkups(markupJSONs);
+  private getFaqsMarkups(faqs) {
+    const markups = {
+      "@context": "https://schema.org",
+      "@type": "FAQPage",
+      "mainEntity": []
+    }
+    for (let faq of faqs) {
+      markups.mainEntity.push({
+        "@type": "Question",
+        "name": faq.content.split(';')[0],
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text": faq.content.split(';')[1]
+        }
+      });
+    }
+    return markups;
   }
 
 }
