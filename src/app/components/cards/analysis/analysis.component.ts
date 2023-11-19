bfb9dd037b654051c1f748e48a02c703a24fa7e1
Javier Suárez <javiersuarsan@gmail.com>
2023-11-19 15:18:51 -0500

    [refactor] from custom selector to section

--- src/app/components/cards/analysis/analysis.component.ts
+++ src/app/components/cards/analysis/analysis.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-analysis',
+  selector: 'section[appAnalysis]',
   templateUrl: './analysis.component.html',
   styleUrls: ['./analysis.component.scss']
 })
